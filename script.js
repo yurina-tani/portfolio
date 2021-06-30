269b4442fff91716117b2fc0e4394a1701543aa0
yurina-tani <84496206+yurina-tani@users.noreply.github.com>
2021-06-30 14:56:41 +0900

    ファイル

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+//ページトップボタン
+window.onscroll = function (event) {
+    //スクロール値の取得
+    const position = document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(position);
+
+
+    //スクロール値が300以上ならクラスopenを追加 未満は消える
+    if (position >= 300) {
+        document.getElementById('page-top').classList.add('open');
+    } else {
+        document.getElementById('page-top').classList.remove('open');
+    }
+};
+
+
+
+
+
+
+
+//ハンバーガーメニュー
+$(".openbtn").click(function () {//ボタンがクリックされたら
+    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+
+
+
+
+
+
+
+//worksのスライドショー
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 5,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
+    responsive: [
+        {
+            breakpoint: 769,//モニターの横幅が769px以下の見せ方
+            settings: {
+                slidesToShow: 2,//スライドを画面に2枚見せる
+                slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+            }
+        },
+        {
+            breakpoint: 426,//モニターの横幅が426px以下の見せ方
+            settings: {
+                slidesToShow: 1,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+            }
+        }
+    ]
+});
